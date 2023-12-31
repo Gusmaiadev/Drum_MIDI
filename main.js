e8f5b44246ec20656040718f72b6141d04445627
Gusmaiadev <gustadev@yahoo.com>
2023-12-31 00:47:28 -0300

    Main

---
+++ main.js
@@ -0,0 +1,36 @@
+function tocaSom (selectAudio) {
+    const elemento =document.querySelector(selectAudio)
+
+    if(elemento != null && elemento.localName === 'audio'){
+        elemento.play();
+    }
+
+    else {
+        console.log('Elemento não encontado');
+    }
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; 
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (event){
+
+        if(event.code === 'Space' || event.code === "Enter" ){
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function (){
+        tecla.classList.remove('ativa');
+    }
+}
